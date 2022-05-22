4ae0c9e775c548e19c56f9c5a5bceeaca308f941
sharkbyte <sgquin@gmail.com>
2022-05-22 08:37:11 -0700

    models finished

--- models/Thought.js
+++ models/Thought.js
@@ -1,7 +1,36 @@
-const { Schema, Types, Model, model } = require("mongoose");
+const { Schema, Types } = require("mongoose");
 const reactionSchema = require("./Reaction");
 
-const thoughtSchema = newSchema({});
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+      default: "I thought therefore I be",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
 
 const Thought = model("thought", thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -1,6 +1,43 @@
 const { Schema, Types, model } = require("mongoose");
 
-const userSchema = new Schema({});
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: `/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/`,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
 
 const User = model("user", userSchema);
 
